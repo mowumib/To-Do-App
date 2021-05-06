e86317cd9d689c2da66f4778e8b95091b5262c66
mowumib <bolutife.odu@gmail.com>
2021-05-06 21:10:01 +0100

    To Do App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "../src/uuid";
+// import { v4 as uuidv4 } from "../src/uuid";
 
 const addButton = document.getElementById('add');
 const input = document.querySelector('.inputValue');
